0987d48b35cf1ec5f91957c19117be761b82f8d6
DenisBSUIR <48877018+DenisBSUIR@users.noreply.github.com>
2020-04-08 00:20:04 +0300

    not working field edit button

--- src/main/java/com/kovalskiy/testproject/controller/FieldsController.java
+++ src/main/java/com/kovalskiy/testproject/controller/FieldsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
@@ -26,6 +27,45 @@ public class FieldsController {
         Iterable<Field> fields = fieldsRepository.findAll();
         model.addAttribute("fields", fields);
         return "/fields";
+    }
+
+    @PostMapping("/changeField")
+    public String changeField(Field field, Model model) {
+
+        Optional<Field> optionalFieldFromDb = fieldsRepository.findById(field.getId());
+        if(optionalFieldFromDb.isPresent()) {
+            Field fieldFromDB = optionalFieldFromDb.get();
+            fieldFromDB.setLabel(field.getLabel());
+            fieldFromDB.setActive(field.isActive());
+            fieldFromDB.setType(field.getType());
+            fieldFromDB.setRequired(field.isRequired());
+
+            HashSet<String> temp = new HashSet<>(field.getOptions());
+            fieldFromDB.getOptions().clear();
+            for (String s: temp) {
+                String[] options = s.split("\n");
+                for(String option: options) {
+                    if (option != null && option != "") {
+                        field.getOptions().add(option);
+                    }
+                }
+            }
+
+            fieldsRepository.save(fieldFromDB);
+        }
+
+        return "redirect:/fields";
+    }
+
+    @PostMapping("/getField")
+    public String getField(@RequestParam String field_id, Model model) {
+        Optional<Field> optionalFieldFromDb = fieldsRepository.findById(Long.parseLong(field_id));
+        if(optionalFieldFromDb.isPresent()) {
+            Field field = optionalFieldFromDb.get();
+            model.addAttribute("fieldForChange", field);
+            model.addAttribute("showModal", false);
+        }
+        return "redirect:/fields";
     }
 
     @PostMapping("/afterLogin")
--- src/main/java/com/kovalskiy/testproject/repository/FieldsRepository.java
+++ src/main/java/com/kovalskiy/testproject/repository/FieldsRepository.java
@@ -4,5 +4,4 @@ import com.kovalskiy.testproject.model.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FieldsRepository extends JpaRepository<Field, Long> {
-
 }
